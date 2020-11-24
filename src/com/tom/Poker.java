6b688f826c3ec6ee994111aeba109c170d2ee010
User <User@ES301N8>
2020-11-24 16:34:02 +0800

    p

--- src/com/tom/Poker.java
+++ src/com/tom/Poker.java
@@ -4,6 +4,7 @@ public class Poker {
 
 	public static void main(String[] args) {
 		int[] cards = new int[52];
+		char[] types = {'S', 'H', 'D', 'C'};
 		for(int i=0; i<52; i++) {
 			cards[i] = i;
 		}
@@ -11,7 +12,8 @@ public class Poker {
 			if(i%13 == 0 && i != 0) {
 				System.out.println();
 			}
-			System.out.print(cards[i] + " ");
+			char flower = types[cards[i]/12];			
+			System.out.print(cards[i] + "" + flower + " ");
 		}
 	}
 
