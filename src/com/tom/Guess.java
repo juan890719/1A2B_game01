6321fb0f5b52f9bff732874c5a714404edc35a32
User <User@ES301N8>
2020-11-24 15:17:02 +0800

    f

---
+++ src/com/tom/Guess.java
@@ -0,0 +1,31 @@
+package com.tom;
+
+import java.util.Random;
+
+public class Guess {
+
+	public static void main(String[] args) {
+		String s ="0";
+		System.out.println((int)s.charAt(0));
+		Random r = new Random();
+		char[] secret = new char[4];
+		for (int i=0; i<4; i++) {
+			char c = (char)(r.nextInt(10)+48);
+			
+			boolean duplicated = false;
+			for(int j=0; j<i; j++) {
+				if(secret[j] == c) {
+					duplicated = true;
+					break;
+				}
+			}
+			if(duplicated) {
+				i--;
+			}else {
+				System.out.print(c);
+				secret[i] = c;
+			}
+		}
+	}
+
+}
